c8220f64ff32c195a6ad073da71ed548790ddef9
REMY ADEDEJI  #RA <remyoreo11@gmail.com>
2025-11-06 07:56:59 -0800

    minor fix;

--- next.config.ts
+++ next.config.ts
@@ -7,16 +7,8 @@ const nextConfig = {
     // This tells Vercel/Next.js to not fail the build if it finds ESLint errors.
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    // This tells webpack to treat this import as an empty module
-    // It resolves the MetaMask SDK build warning
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "@react-native-async-storage/async-storage": false,
-    };
 
-    return config;
-  },
+
 
 };
 
